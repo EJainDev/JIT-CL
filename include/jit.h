8db3bf2b89d4541801ea904fb6bc75a7ee7ce9db
EJainDev <ekansh.jain2011@gmail.com>
2026-03-13 17:17:28 -0400

    Update indexing

--- include/jit.h
+++ include/jit.h
@@ -15,68 +15,88 @@ namespace jitcl {
 
 inline auto genKernel(cl::Context context, const JitState state,
                       const std::vector<JitTracer>& params) -> cl::Kernel {
-  std::string kernel = R"(
-    __kernel void kernel(
-  )";
+  std::string kernel = "__kernel void kern(";
   for (const auto& tracer : params) {
     // Parameter
-    kernel += std::format("__global float *{}, ", tracer._data->name);
+    kernel += std::format(
+        "__global float *{0}, __global int *{0}_shape, __global int *{0}_stride, int {0}_dims",
+        tracer._data->name);
   }
   kernel = kernel.substr(0, kernel.size() - 2);  // Remove the last ", "
-  kernel += ") {\nconst int gid0 = get_global_id(0);\nconst int global0 = get_global_size(0);\n";
+  kernel += R"() {
+const int gid0 = get_global_id(0);
+const int gid1 = get_global_id(1);
+const int global0 = get_global_size(0);
+const int global1 = get_global_size(1);
+)";
+  for (const auto& tracer : params) {
+    kernel += std::format("if (gid0 >= {0}_shape[0] || gid1 >= {0}_shape[1]) return;\n",
+                          tracer._data->name);
+  }
   for (const auto& operation : state.get()->operations) {
     switch (operation.op) {
       case internal::Operations::None:
         break;
       case internal::Operations::Assign: {
-        kernel += std::format("{}[gid0] = {}[gid0];\n", operation.lhs._data->name,
-                              operation.rhs._data->name);
+        kernel += std::format("{0}[gid1 * {0}_stride[gid0]] = {1}[gid1 * {0}_stride[gid0]];\n",
+                              operation.lhs._data->name, operation.rhs._data->name);
         break;
       }
       case internal::Operations::Add: {
-        kernel += std::format("{}[gid0] = {}[gid0] + {}[gid0];\n", operation.output._data->name,
-                              operation.lhs._data->name, operation.rhs._data->name);
+        kernel += std::format(
+            "{0}[gid1 * {0}_stride[gid0]] = {1}[gid1 * {1}_stride[gid0]] + {2}[gid1 * "
+            "{2}_stride[gid0]];\n",
+            operation.output._data->name, operation.lhs._data->name, operation.rhs._data->name);
         break;
       }
       case internal::Operations::Subtract: {
-        kernel += std::format("{}[gid0] = {}[gid0] - {}[gid0];\n", operation.output._data->name,
-                              operation.lhs._data->name, operation.rhs._data->name);
+        kernel += std::format(
+            "{0}[gid1 * {0}_stride[gid0]] = {1}[gid1 * {1}_stride[gid0]] - {2}[gid1 * "
+            "{2}_stride[gid0]];\n",
+            operation.output._data->name, operation.lhs._data->name, operation.rhs._data->name);
         break;
       }
       case internal::Operations::Multiply: {
-        kernel += std::format("{}[gid0] = {}[gid0] * {}[gid0];\n", operation.output._data->name,
-                              operation.lhs._data->name, operation.rhs._data->name);
+        kernel += std::format(
+            "{0}[gid1 * {0}_stride[gid0]] = {1}[gid1 * {1}_stride[gid0]] * {2}[gid1 * "
+            "{2}_stride[gid0]];\n",
+            operation.output._data->name, operation.lhs._data->name, operation.rhs._data->name);
         break;
       }
       case internal::Operations::Divide: {
-        kernel += std::format("{}[gid0] = {}[gid0] / {}[gid0];\n", operation.output._data->name,
-                              operation.lhs._data->name, operation.rhs._data->name);
+        kernel += std::format(
+            "{0}[gid1 * {0}_stride[gid0]] = {1}[gid1 * {1}_stride[gid0]] / {2}[gid1 * "
+            "{2}_stride[gid0]];\n",
+            operation.output._data->name, operation.lhs._data->name, operation.rhs._data->name);
         break;
       }
       case internal::Operations::And: {
-        kernel +=
-            std::format("{}[gid0] = (int){}[gid0] & (int){}[gid0];\n", operation.output._data->name,
-                        operation.lhs._data->name, operation.rhs._data->name);
+        kernel += std::format(
+            "{0}[gid1 * {0}_stride[gid0]] = (int){1}[gid1 * {1}_stride[gid0]] & (int){2}[gid1 * "
+            "{2}_stride[gid0]];\n",
+            operation.output._data->name, operation.lhs._data->name, operation.rhs._data->name);
         break;
       }
       case internal::Operations::Or: {
-        kernel +=
-            std::format("{}[gid0] = (int){}[gid0] | (int){}[gid0];\n", operation.output._data->name,
-                        operation.lhs._data->name, operation.rhs._data->name);
+        kernel += std::format(
+            "{0}[gid1 * {0}_stride[gid0]] = (int){1}[gid1 * {1}_stride[gid0]] | (int){2}[gid1 * "
+            "{2}_stride[gid0]];\n",
+            operation.output._data->name, operation.lhs._data->name, operation.rhs._data->name);
         break;
       }
       case internal::Operations::Xor: {
-        kernel +=
-            std::format("{}[gid0] = (int){}[gid0] ^ (int){}[gid0];\n", operation.output._data->name,
-                        operation.lhs._data->name, operation.rhs._data->name);
+        kernel += std::format(
+            "{0}[gid1 * {0}_stride[gid0]] = (int){1}[gid1 * {1}_stride[gid0]] ^ (int){2}[gid1 * "
+            "{2}_stride[gid0]];\n",
+            operation.output._data->name, operation.lhs._data->name, operation.rhs._data->name);
         break;
       }
       case internal::Operations::Reduction: {
         kernel += std::format(R"(float reduce_var = 0.0;
-for (int i = global0 - 1; i > 0; i >>= 1) {{
-if (gid0 < i) {{
-{0}[gid0] += {0}[gid0 + i];
-}}
+for (int i = global1 - 1; i > 0; i >>= 1) {{
+  if (gid1 < i) {{
+    {0}[gid1 * {0}_stride[gid0]] += {0}[gid1 * {0}_stride[gid0]] + i];
+  }}
 }})",
                               operation.lhs._data->name);
       }
@@ -89,7 +109,7 @@ if (gid0 < i) {{
 
   cl::Program program(context, kernel);
   program.build();
-  return {program, "kernel"};
+  return {program, "kern"};
 }
 }  // namespace jitcl
 
